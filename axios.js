c17bb3609c19a780535877dc5b0adb17db4facf0
akjurysoft <akash.k@jurysoft.com>
2024-05-15 15:10:26 +0530

    made change

--- axios.js
+++ axios.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4040/',
-    // baseURL: 'http://54.159.106.52:4040/',
-    headers: {
-        post: {
-            "Accept": 'application/json',
-            "Content-Type": "application/json",
-        },
-        get: {
-            "Accept": 'application/json',
-            "Content-Type": "application/json",
-        },
+  baseURL: "http://localhost:4040/",
+  // baseURL: 'http://54.159.106.52:4040/',
+  headers: {
+    post: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    withCredentials: false,
-})
+    get: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  },
+  withCredentials: false,
+});
 
 export default instance;
